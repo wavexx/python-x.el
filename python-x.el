724d8df5288e98503c4fea120bcdddc9e3ab919d
Yuri D'Elia <yuri.delia@eurac.edu>
2015-07-13 15:14:41 +0200

    Add documentation/commentary.

--- python-x.el
+++ python-x.el
@@ -25,7 +25,34 @@
 
 ;;; Commentary:
 
-;; TODO
+;; python-x extends the built-in `python-mode' (F. Gallina's) with several
+;; additional functions and behaviors inspired by `ess-mode', which are
+;; targeted to interactive code evaluation with an inferior Python process.
+;;
+;; python-x allows to evaluate code blocks using comments as delimiters (code
+;; "sections") or using arbitrarily nested folding marks.
+;;
+;; python-x installs an handler to show uncaught exceptions produced by
+;; interactive code evaluation by default. See `python-shell-show-exceptions'
+;; to control this behavior.
+;;
+;; The following functions are introduced:
+;; - `python-shell-send-line': evaluate and print the current line, accounting
+;;   for statement and line continuations.
+;; - `python-shell-send-line-and-step': evaluate and current line as above,
+;;   then move the point to the next automatically.
+;; - `python-shell-send-fold-or-section': evaluate the region defined by the
+;;   current code fold or section.
+;; - `python-shell-send-dwim': evaluate the active region when active,
+;;   otherwise revert to the current fold or section.
+;;
+;; python-x uses `volatile-highlights' for highlighting. Installation through
+;; "melpa" is recommended (you don't actually need to enable
+;; `volatile-highlights-mode' itself). `folding-mode' is similarly required (on
+;; Debian it can be installed through the "emacs-goodies-el" package).
+;;
+;; Read through the Usage section in the source file for usage instructions and
+;; recommended keybindings.
 
 ;;; Usage:
 
@@ -90,16 +117,18 @@ printing the result of the expression on the shell, then move on to the next sta
 
 ;;;###autoload
 (defcustom python-section-delimiter "# ---"
-  "TODO"
+  "Define the comment which marks the boundaries of the current code section.
+See `python-shell-send-fold-or-section'."
   :type 'string
   :group 'python-x)
 
 ;;;###autoload
 (defcustom python-section-highlight (if (require 'volatile-highlights nil t) t)
-  "TODO"
+  "When evaluating a code fold/section with `python-shell-send-fold-or-section'
+spanning more than one line, highlight temporarily the evaluated region using
+`vhl/default-face'. Requires `volatile-highlights-mode' to be installed."
   :type 'boolean
   :group 'python-x)
-
 
 (defun python-section-in-skiplist (pos skip)
   (if (not skip) nil
@@ -131,7 +160,24 @@ printing the result of the expression on the shell, then move on to the next sta
 
 ;;;###autoload
 (defun python-shell-send-fold-or-section ()
-  "TODO"
+  "Send the section of code at point to the inferior Python process, up to the
+current fold or buffer boundaries.
+
+A code \"section\" is delimited in both directions, and in order, by:
+
+- The nearest section delimiter (see `python-section-delimiter') contained
+  within the current fold.
+- The nearest fold delimiter (see `folding-mode-marks-alist').
+- The buffer boundaries.
+
+`folding-mode' doesn't need to be enabled, but the same marks are used to
+define code boundaries. See `folding-add-to-marks-list' for customization.
+Nested folds and sections are included: section delimiters contained within a
+nested fold are ignored.
+
+When the region to be evaluated is longer than a single line and less than a
+screenful, the region is temporarily highlighted according to
+`python-section-highlight'."
   (interactive)
   (unless folding-regexp
     ;; define folding markers, even when folding-mode is not active
@@ -168,11 +214,14 @@ Otherwise, use `python-shell-send-current-fold-or-section'"
 ;; Exception handling
 
 (defcustom python-shell-show-exceptions t
-  "TODO"
+  "Display uncaught exceptions of the inferior Python process using
+`python-shell-show-exceptions'."
   :type 'boolean
   :group 'python-x)
 
 (defun python-shell-show-exception-function (buffer)
+  "Function invoked when the inferion Python process emits an uncaught
+exception. Calls `display-buffer' according to `python-shell-show-exceptions'."
   (when python-shell-show-exceptions
     (display-buffer buffer)))
 
@@ -189,3 +238,5 @@ Otherwise, use `python-shell-send-current-fold-or-section'"
 		      'python-comint-find-exceptions)))
 
 (provide 'python-x)
+
+;;; python-x.el ends here
